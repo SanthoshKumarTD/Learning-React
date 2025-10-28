9e58536c3168f6fb376af16492d46ba81b972f74
SanthoshKumarTD <santoshpatiltd@gmail.com>
2025-10-28 16:58:31 +0530

    first commit

---
+++ 01-Folders/Folders/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div id="Dad">
+      <h1>Santhosh</h1>
+     </div>
+     <div id='Mom'>
+      <h1>Kruthika</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
