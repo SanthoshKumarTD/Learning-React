f63cb9286aa640fdb5f76560b05f51d395839b46
SanthoshKumarTD <santoshpatiltd@gmail.com>
2025-11-03 16:31:35 +0530

    third  commit

---
+++ 04-CardsPrject/src/Components/Users.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Users = (props) => {
+  return (
+    <div>
+      Santosh {props.id}
+    </div>
+  )
+}
+
+export default Users
---
+++ 06-TailWindCSS/src/App.jsx
@@ -0,0 +1,18 @@
+import { HeartOff } from 'lucide-react'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>TailWindCDD</h1>
+      <h3><p>A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.</p></h3>
+      <h2>Website: https://tailwindcss.com/ </h2>
+      <p>click on <b>get started</b> and navigate to <b>Using vite</b> 
+      and copy the installation guide. this is the new CSS tool. 
+      And older CSS tool was bootstarp. 
+      Extenstion name to use it in visual studio code is TailWindCSS Intellisense</p>
+    </div>
+  )
+}
+
+export default App
