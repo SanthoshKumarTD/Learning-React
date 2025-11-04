0a51c9391f671cd5111f0cf47d542927becb4cb5
SanthoshKumarTD <santoshpatiltd@gmail.com>
2025-11-04 12:00:23 +0530

    third  commit

--- 06-TailWindCSS/src/App.jsx
+++ 06-TailWindCSS/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     <div>
       <h1>TailWindCDD</h1>
       <h3><p>A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.</p></h3>
-      <h2>Website: https://tailwindcss.com/ </h2>
+      <h2>Website: https://tailwindcss.com/</h2>      
       <p>click on <b>get started</b> and navigate to <b>Using vite</b> 
       and copy the installation guide. this is the new CSS tool. 
       And older CSS tool was bootstarp. 
